01aa8f0f14409b69bdc3111b4689d858b710f1ef
emmelinlarina <emmelin.tvedt@gmail.com>
2025-09-19 14:57:39 +0200

    Checkout css done

--- script/checkout.js
+++ script/checkout.js
@@ -82,7 +82,11 @@ function render() {
     }
 
     const rows = cart.map(itemHTML).join("");
-    shopWrap.innerHTML = `<h1>Your Cart</h1>${rows}`;
+    shopWrap.innerHTML = `
+    <img src="images/logo/NovaCart_brown_cropped.png" alt="Novacart logo" class="cart-logo">
+    <h1>Your Cart</h1>${rows}` 
+    ;
+    
 
     wire();
     renderTotals();
